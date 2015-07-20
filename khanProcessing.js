d431d312085fad0fb3d860eb61b9a5aa8c3ab96b
thinsoldier <thinsoldier@thinsoldier.com>
2015-07-20 17:01:57 -0400

    Rearranged code to make it a reusable function.

---
+++ khanProcessing.js
@@ -0,0 +1,37 @@
+//http://stackoverflow.com/questions/25341597/how-can-khan-academy-computer-programs-be-run-offline-or-on-my-own-website
+//http://stackoverflow.com/a/25341598/1154693
+function khanProcessing( programFunction, canvasElement ) 
+{
+	var canvas = canvasElement;
+	var processing = new Processing(canvas, function(processing) {
+		processing.size(400, 400);
+		processing.background(0xFFF);
+
+		var mouseIsPressed = false;
+		processing.mousePressed = function () { mouseIsPressed = true; };
+		processing.mouseReleased = function () { mouseIsPressed = false; };
+
+		var keyIsPressed = false;
+		processing.keyPressed = function () { keyIsPressed = true; };
+		processing.keyReleased = function () { keyIsPressed = false; };
+
+		function getImage(s) {
+			var url = "https://www.kasandbox.org/programming-images/" + s + ".png";
+			processing.externals.sketch.imageCache.add(url);
+			return processing.loadImage(url);
+		}
+
+		with (processing) {
+//-----------------------------
+// INSERT YOUR KHAN ACADEMY PROGRAM BELOW
+//-----------------------------
+
+programFunction(processing);
+
+//-----------------------------
+// END OF YOUR KHAN ACADEMY PROGRAM
+//-----------------------------
+		}
+		if (typeof draw !== 'undefined') processing.draw = draw;
+	});
+};
